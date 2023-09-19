14043f020a8e6a5fde4df56f978785f8aee4fe1b
tanzeel ahmed <tanzilsapra786@gmail.com>
2023-09-19 21:42:43 +0500

    Initial commit

---
+++ pyhon.py
@@ -0,0 +1,12 @@
+import time  
+print(time.strftime("%Y-%m-%d %I:%M:%S %p",time.localtime()))
+x = time.localtime()
+if 4 < x.tm_hour  < 12 : 
+    print("Good morning.Sir!")
+elif 12 < x.tm_hour < 18 :
+    print("Good afternoon.Sir!")
+elif 18 < x.tm_hour < 21 :
+    print("Good evening.Sir!")
+else :
+    print("It's night.Sir!")
+
